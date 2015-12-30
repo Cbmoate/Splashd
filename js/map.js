6061e5cdf7f5700e63912279d7d6f9b987d47220
Rolando Cosme <rolandocosme@Rolandos-MacBook-Pro.local>
2015-12-29 19:51:54 -0500

    added code for current location on maps.js

--- js/map.js
+++ js/map.js
@@ -1,9 +1,12 @@
+var initialLocation;
+var rutgers = new google.maps.LatLng(40.525751, -74.438212);
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var browserSupportFlag =  new Boolean();
+// var myLatLng = {lat: 40.525751, lng: -74.438212};
 
 
 function initMap() {
   var myLatLng = {lat: 40.525751, lng: -74.438212};
-
-
   var map = new google.maps.Map(document.getElementById("resultMap"), {
     zoom: 15,
     center: myLatLng
@@ -12,7 +15,43 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    title: "Google Map Results"
-  });
+    title: "Rutgers"
+  })
 }
 
+
+
+function initialize() {
+  var myOptions = {
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("resultmap"), myOptions);
+
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = myLatLng;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = myLatLng;
+    }
+    map.setCenter(initialLocation);
+  }
+}
