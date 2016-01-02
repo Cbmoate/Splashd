64da72db5d5e3e8ea917d7e0723f094d9e7569c3
CB <cbmoate@gmail.com>
2016-01-02 15:30:34 -0500

    added beginning of firebase JS, removed IMAGES folder

--- js/firebase.js
+++ js/firebase.js
@@ -1,2 +1,10 @@
 
 var myDataRef = new Firebase('https://va3izfedini.firebaseio-demo.com/');
+var locationsRef = myDataRef.child('locations')
+
+locationRef.set({
+  long, lat:{
+    rating: "4"
+    comment: "This bathroom is pretty okay"
+  }
+});
