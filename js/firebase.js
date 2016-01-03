2504669d6b9c2bc6026d961a8d79a435017bd0ac
CB <cbmoate@gmail.com>
2016-01-03 12:40:17 -0500

    fixed the toilet paper image, minor changes to firebase JS

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,7 @@ var myDataRef = new Firebase('https://va3izfedini.firebaseio-demo.com/');
 var locationsRef = myDataRef.child('locations')
 
 locationRef.set({
-  long, lat:{
+  lon, lat:{
     rating: "4"
     comment: "This bathroom is pretty okay"
   }
