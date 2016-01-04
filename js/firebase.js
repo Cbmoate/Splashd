90070fdc621d1a0695369c17bff569cf3833bf9c
CB <cbmoate@gmail.com>
2016-01-04 12:06:28 -0500

    firebase code coming along

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,12 @@
 var myDataRef = new Firebase('https://va3izfedini.firebaseio-demo.com/');
 var locationsRef = myDataRef.child('locations')
 
+$(".btn btn-primary").on("click", function comments(){
+  .preventDefault();
+  var comment = $(".wallComment")
+  var location = $
+}
+
 locationRef.set({
   lon, lat:{
     rating: "4"
